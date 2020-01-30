68249e318403bef1c0dafd6d53a136343b6aa40a
benja971 <46868460+benja971@users.noreply.github.com>
2020-01-30 19:50:56 -0400

    Add files via upload

--- jeuClass.py
+++ jeuClass.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame, random, os
 from pygame.locals import *
 
 from ctypes import windll
@@ -6,7 +6,7 @@ windll.shcore.SetProcessDpiAwareness(1)
 
 pygame.init()
 
-
+os.system('cls')
 
 class Perso:
 	def __init__(self):
@@ -15,6 +15,7 @@ class Perso:
 		self.rect = self.img.get_rect()
 		self.rect.x = 102
 		self.rect.y = 102
+		self.vies = 10
 
 
 	def move(self, touches):
@@ -35,13 +36,15 @@ class Perso:
 class Enemy:
 	def __init__(self):
 		self.img = bank["mort"]
-		self.v = 3
+		self.v = 1
 		self.rect = self.img.get_rect()
-		self.rect.x = 500
-		self.rect.y = 500
+		self.rect.x = random.randint(200, 1062)
+		self.rect.y = random.randint(100, 532)
 		
 	def collisions(self, perso):
+
 		return self.rect.colliderect(perso.rect)
+
 
 	def move(self, perso):
 		if perso.rect.x < self.rect.x:
@@ -64,52 +67,147 @@ horloge = pygame.time.Clock()
 bank = {
 	"perso": pygame.image.load("Dragon1.png").convert_alpha(),
 	"mort": pygame.image.load("mort.png").convert_alpha(),
-	"fond": pygame.image.load("bg2-bell.jpg").convert()
+	"fond": pygame.image.load("Fond d'écran .jpg").convert_alpha(),
+
+	"vie100": pygame.image.load("vie100.png").convert_alpha(),
+	"vie90": pygame.image.load("vie90.png").convert_alpha(),
+	"vie80": pygame.image.load("vie80.png").convert_alpha(),
+	"vie70": pygame.image.load("vie70.png").convert_alpha(),
+	"vie60": pygame.image.load("vie60.png").convert_alpha(),
+	"vie50": pygame.image.load("vie50.png").convert_alpha(),
+	"vie40": pygame.image.load("vie40.png").convert_alpha(),
+	"vie30": pygame.image.load("vie30.png").convert_alpha(),
+	"vie20": pygame.image.load("vie20.png").convert_alpha(),
+	"vie10": pygame.image.load("vie10.png").convert_alpha(),
 }
 
-continuer = True
-pygame.key.set_repeat(4, 4)
-
 perso = Perso()
+enemy = Enemy()
 
+font2 = pygame.font.Font('RioGrande.ttf', 55)
+
+txtSingleplayer = font2.render("Singleplayer", 1, (255, 0, 0))
+SingleplayerRect = Rect(1262/2 - SingleplayerRect.w/2, 2*(632/8)-20))
+print(*SingleplayerRect)
+
+txtMultiplayer = font2.render("Multiplayer", 1, (255, 0, 0))
+MultiplayerRect = txtMultiplayer.get_rect()
+
+txtInfos = font2.render("Informations", 1, (255, 0, 0))
+InfosRect = txtInfos.get_rect()
+
+txtQuit = font2.render("Exit", 1, (255, 0, 0))
+QuitRect = txtQuit.get_rect()
+
+test = bank["perso"].get_rect()
+
+
+continuer = True
+state = 'menu'
 i = 0
 enemyList = []
 
+enemyList.append(Enemy())
+
+pygame.key.set_repeat(4, 4)
+
 while continuer:
-	horloge.tick(60)
+	horloge.tick(120)
 	i += 1
 
 	touches = pygame.key.get_pressed()
 	events = pygame.event.get()
 
-	perso.move(touches)
-
-	for enemy in enemyList:
-		enemy.move(perso)
-
-	if touches[K_ESCAPE]:
-		continuer = False
-	
-
-	if i%100 is 0:
-		new = True
-		for enemy in enemyList:
-			if enemy.v < 5:
-				enemy.v += 1
-				new = False
-
-		# si tous les ennemis ont une vitesse de 5 ou
-		# s'il n'y en a pas alors on ajoue un ennemi
-		if new:
-			enemyList.append(Enemy())
-
-
-
 	fenetre.blit(bank["fond"], (0, 0))
-	fenetre.blit(bank["perso"], perso.rect)
 
-	for enemy in enemyList:
-		# print(enemyList, end = '\r')
-		fenetre.blit(bank["mort"], enemy.rect)
+	if state == 'menu':
 
-	pygame.display.update()	
+		if touches[K_ESCAPE]:
+			continuer = False
+
+		if test.colliderect(SingleplayerRect):
+			print("touche")
+
+		for event in pygame.event.get():
+			if event.type == MOUSEMOTION:
+				test.x = event.pos[0]
+				test.y = event.pos[1]
+
+		fenetre.blit(txtSingleplayer, (1262/2 - SingleplayerRect.w/2, 2*(632/8)-20))
+		fenetre.blit(txtMultiplayer, (1262/2 - MultiplayerRect.w/2, 3*(632/8)+10))
+		fenetre.blit(txtInfos, (1262/2 - InfosRect.w/2, 4*(632/8)+40))
+		fenetre.blit(txtQuit, (1262/2 - QuitRect.w/2, 5*(632/8)+60))
+		fenetre.blit(bank["perso"], (test.x - test.w, test.y))
+
+
+	if state == 'Singleplayer':
+		
+		perso.move(touches)
+		enemy.collisions(perso)
+
+		for enemy in enemyList:
+			enemy.move(perso)
+
+		if touches[K_ESCAPE]:
+			continuer = False
+		
+
+		if i%1000 is 0:
+			new = True
+			for enemy in enemyList:
+				if enemy.v < 4:
+					enemy.v += 1
+					new = False
+
+			# si tous les ennemis ont une vitesse de 5 ou
+			# s'il n'y en a pas alors on ajoue un ennemi
+			if new:
+				enemyList.append(Enemy())
+
+		for enemy in enemyList:
+			if enemy.rect.colliderect(perso.rect):
+				perso.vies -= 1
+				perso.rect.x = random.randint(200, 1062)
+				perso.rect.y = random.randint(100, 532)
+
+
+		fenetre.blit(bank["perso"], perso.rect)
+
+		if perso.vies == 10:
+			fenetre.blit(bank["vie100"], (10,10))
+
+		if perso.vies == 9:
+			fenetre.blit(bank["vie90"], (10,10))
+
+		if perso.vies == 8:
+			fenetre.blit(bank["vie80"], (10,10))
+
+		if perso.vies == 7:
+			fenetre.blit(bank["vie70"], (10,10))
+
+		if perso.vies == 6:
+			fenetre.blit(bank["vie60"], (10,10))
+
+		if perso.vies == 5:
+			fenetre.blit(bank["vie50"], (10,10))
+
+		if perso.vies == 4:
+			fenetre.blit(bank["vie40"], (10,10))
+
+		if perso.vies == 3:
+			fenetre.blit(bank["vie30"], (10,10))
+		
+		if perso.vies == 2:
+			fenetre.blit(bank["vie20"], (10,10))
+
+		if perso.vies == 1:
+			fenetre.blit(bank["vie10"], (10,10))
+
+		if perso.vies == 0:
+			state = 'menu'
+
+		for enemy in enemyList:
+			# print(enemyList, end = '\r')
+			fenetre.blit(bank["mort"], enemy.rect)
+
+	pygame.display.flip()	
