0dfffd141d2564af6b444a6c46dffa6b84757b13
NIDDAM <niddam.benjamin@orange.fr>
2020-01-30 21:22:34 -0400

    v 2.1
    Menu utilisable avec la sourie uniquement + petit début de sons

--- jeuClass.py
+++ jeuClass.py
@@ -64,10 +64,28 @@ class Enemy:
 fenetre = pygame.display.set_mode((1262, 632))
 horloge = pygame.time.Clock()
 
+font2 = pygame.font.Font('RioGrande.ttf', 55)
+
+txtSingleplayer = font2.render("Singleplayer", 1, (255, 0, 0))
+SingleplayerRect = Rect(450, 140, 365, 55)
+
+txtMultiplayer = font2.render("Multiplayer", 1, (255, 0, 0))
+MultiplayerRect = Rect(455, 255, 350, 55)
+
+txtInfos = font2.render("Informations", 1, (255, 0, 0))
+InfosRect = Rect(440, 365, 385, 35)
+
+txtQuit = font2.render("Exit", 1, (255, 0, 0))
+QuitRect = Rect(570, 460, 120, 40)
+
+sonswitchmemu = pygame.mixer.Sound("1978.wav")
+
+
 bank = {
 	"perso": pygame.image.load("Dragon1.png").convert_alpha(),
 	"mort": pygame.image.load("mort.png").convert_alpha(),
-	"fond": pygame.image.load("Fond d'écran .jpg").convert_alpha(),
+	"fondM": pygame.image.load("background.jpg").convert_alpha(),
+	"fondJ": pygame.image.load("Fond d'écran .jpg").convert_alpha(),
 
 	"vie100": pygame.image.load("vie100.png").convert_alpha(),
 	"vie90": pygame.image.load("vie90.png").convert_alpha(),
@@ -84,29 +102,13 @@ bank = {
 perso = Perso()
 enemy = Enemy()
 
-font2 = pygame.font.Font('RioGrande.ttf', 55)
-
-txtSingleplayer = font2.render("Singleplayer", 1, (255, 0, 0))
-SingleplayerRect = Rect(1262/2 - SingleplayerRect.w/2, 2*(632/8)-20))
-print(*SingleplayerRect)
-
-txtMultiplayer = font2.render("Multiplayer", 1, (255, 0, 0))
-MultiplayerRect = txtMultiplayer.get_rect()
-
-txtInfos = font2.render("Informations", 1, (255, 0, 0))
-InfosRect = txtInfos.get_rect()
-
-txtQuit = font2.render("Exit", 1, (255, 0, 0))
-QuitRect = txtQuit.get_rect()
-
-test = bank["perso"].get_rect()
-
-
 continuer = True
 state = 'menu'
 i = 0
-enemyList = []
+x = 0
+y = 0
 
+enemyList = []
 enemyList.append(Enemy())
 
 pygame.key.set_repeat(4, 4)
@@ -118,29 +120,67 @@ while continuer:
 	touches = pygame.key.get_pressed()
 	events = pygame.event.get()
 
-	fenetre.blit(bank["fond"], (0, 0))
 
 	if state == 'menu':
+		fenetre.blit(bank["fondM"], (0, 0))
+		select = Rect(x, y, 1, 1)
 
 		if touches[K_ESCAPE]:
 			continuer = False
 
-		if test.colliderect(SingleplayerRect):
-			print("touche")
-
 		for event in pygame.event.get():
-			if event.type == MOUSEMOTION:
-				test.x = event.pos[0]
-				test.y = event.pos[1]
+			if select.colliderect(SingleplayerRect):
+				if event.type == MOUSEBUTTONDOWN:
+					sonswitchmemu.play()
+					state = 'Singleplayer'
 
-		fenetre.blit(txtSingleplayer, (1262/2 - SingleplayerRect.w/2, 2*(632/8)-20))
+			if select.colliderect(MultiplayerRect):
+				if event.type == MOUSEBUTTONDOWN:
+					sonswitchmemu.play()
+					state = 'Multiplayer'
+
+			if select.colliderect(InfosRect):
+				if event.type == MOUSEBUTTONDOWN:
+					sonswitchmemu.play()
+					print("Infos.....")
+
+			if select.colliderect(QuitRect):
+				if event.type == MOUSEBUTTONDOWN:
+					sonswitchmemu.play()
+					continuer = False
+
+
+			if event.type == MOUSEMOTION:
+				x = event.pos[0]
+				y = event.pos[1]
+
+		if select.colliderect(SingleplayerRect):
+			txtSingleplayer = font2.render("Singleplayer", 1, (255, 255, 255))
+		else:
+			txtSingleplayer = font2.render("Singleplayer", 1, (255, 0, 0))
+
+		if select.colliderect(MultiplayerRect):
+			txtMultiplayer = font2.render("Multiplayer", 1, (255, 255, 255))
+		else:
+			txtMultiplayer = font2.render("Multiplayer", 1, (255, 0, 0))
+
+		if select.colliderect(InfosRect):
+			txtInfos = font2.render("Informations", 1, (255, 255, 255))
+		else:
+			txtInfos = font2.render("Informations", 1, (255, 0, 0))
+
+		if select.colliderect(QuitRect):
+			txtQuit = font2.render("Exit", 1, (255, 255, 255))
+		else:
+			txtQuit = font2.render("Exit", 1, (255, 0, 0))
+
+		fenetre.blit(txtSingleplayer, (450, 140))
 		fenetre.blit(txtMultiplayer, (1262/2 - MultiplayerRect.w/2, 3*(632/8)+10))
 		fenetre.blit(txtInfos, (1262/2 - InfosRect.w/2, 4*(632/8)+40))
 		fenetre.blit(txtQuit, (1262/2 - QuitRect.w/2, 5*(632/8)+60))
-		fenetre.blit(bank["perso"], (test.x - test.w, test.y))
-
 
 	if state == 'Singleplayer':
+		fenetre.blit(bank["fondJ"], (0, 0))
 		
 		perso.move(touches)
 		enemy.collisions(perso)
@@ -151,7 +191,6 @@ while continuer:
 		if touches[K_ESCAPE]:
 			continuer = False
 		
-
 		if i%1000 is 0:
 			new = True
 			for enemy in enemyList:
@@ -169,7 +208,6 @@ while continuer:
 				perso.vies -= 1
 				perso.rect.x = random.randint(200, 1062)
 				perso.rect.y = random.randint(100, 532)
-
 
 		fenetre.blit(bank["perso"], perso.rect)
 
@@ -207,7 +245,6 @@ while continuer:
 			state = 'menu'
 
 		for enemy in enemyList:
-			# print(enemyList, end = '\r')
 			fenetre.blit(bank["mort"], enemy.rect)
 
 	pygame.display.flip()	
